c8db7192f62c3a061bc7df7471d67e58f568a5ba
PalwinderK <w0771480@myscc.ca>
2021-11-11 11:48:15 -0500

    Assignment-6

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  const allPanels = $(".accordion > dd").hide();
+
+  $(".accordion > dt > a").click(function () {
+    allPanels.slideUp();
+    $(this).parent().next().slideDown();
+    return false;
+  });
+});
